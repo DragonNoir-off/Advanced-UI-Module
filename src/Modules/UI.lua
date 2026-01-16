8c395386103b6c554f431f99150a51b6a855fffb
DragonNoir <clement.lab78@gmail.com>
2026-01-16 13:45:48 +0100

    Update UI.lua

--- src/Modules/UI.lua
+++ src/Modules/UI.lua
@@ -8,6 +8,28 @@ end
 function module.new(ui_data)
     local self = setmetatable({}, module)
     
+    --[[ ui_data data info
+    
+    1 / name : string
+    2 / zindex : number
+    3 / size : {scale-x,offset-x,scale-y,offset-y}
+    4 / parent : object
+    5 / childs : {object}
+    6 / self-visible : bool
+    7 / child-visible : bool
+    8 / attribut : {
+        -- global
+        1 / ui_type : string
+
+        -- circle
+        2 / color : {number,number,number,number} ( 0-1 )
+        
+        -- rectangle
+        2 / color : {number,number,number,number} ( 0-1 )
+        3 / corner_radius : {number,number,number,number} ( 0-1 )
+    }
+
+    ]]
 end
 
 -- SET DATA TO OBJECT
